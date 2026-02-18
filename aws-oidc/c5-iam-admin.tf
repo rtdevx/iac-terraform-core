945d15a0e151e9a873a20e8aed466cbc0127952c
robk <robk@localhost>
2026-02-18 17:35:33 +0000

    [HL-54] Applying OIDC policies - DetachGroupPolicy

--- aws-oidc/c5-iam-admin.tf
+++ aws-oidc/c5-iam-admin.tf
@@ -2,14 +2,14 @@
 # INFO: Attach "AdministratorAccess" policy to "admin" group.                                                         
 # INFO: ##############################################################################################################
 
-/*
+
 
 resource "aws_iam_group_policy_attachment" "admin_AdministratorAccess" {
   group      = "admin"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-/*
+
 
 /*
 
