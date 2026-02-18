2a18a18045dbbb29244f715c43bae4df2b43a509
robk <robk@localhost>
2026-02-18 16:03:52 +0000

    [HL-54] Applying OIDC policies - admin

--- aws-oidc/c5-iam-admin.tf
+++ aws-oidc/c5-iam-admin.tf
@@ -2,8 +2,6 @@
 # INFO: Attach "AdministratorAccess" policy to "admin" group. Should only be used for manual interventions or testing.
 # INFO: ##############################################################################################################
 
-/*
-
 resource "aws_iam_group_policy_attachment" "admin_AdministratorAccess" {
   group      = "admin"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
@@ -13,5 +11,3 @@ resource "aws_iam_group_policy_attachment" "test-attach" {
   group      = "admin"
   policy_arn = "arn:aws:iam::aws:policy/AlexaForBusinessReadOnlyAccess"
 }
-
-*/
