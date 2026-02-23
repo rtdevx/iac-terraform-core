695ae81e71f34976893746d56810acab80db7642
robk <robk@localhost>
2026-02-23 11:52:20 +0000

    [HL-46] Updating IAM roles to allow GH Actions create SSM roles

--- aws-oidc/c6-iam-infra-jvx.tf
+++ aws-oidc/c6-iam-infra-jvx.tf
@@ -229,6 +229,7 @@ resource "aws_iam_role_policy" "oidc_policy_infra_jvx" {
           "iam:GetRolePolicy",
           "iam:DeleteRolePolicy",
           "iam:AddRoleToInstanceProfile",
+          "iam:PassRole",
           # ACM
           "acm:RequestCertificate",
           "acm:DescribeCertificate",
