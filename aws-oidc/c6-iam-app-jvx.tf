a8f70cd80df2b1e3dfc51f836d9e8f037a8b3fe1
robk <robk@localhost>
2026-05-20 15:37:47 +0100

    [HL-68] FIX: OICD autoscaling:DescribeInstanceRefreshes

--- aws-oidc/c6-iam-app-jvx.tf
+++ aws-oidc/c6-iam-app-jvx.tf
@@ -105,7 +105,8 @@ resource "aws_iam_role_policy" "oidc_policy_aws_jvx" {
 
         Resource = [ 
           "arn:aws:autoscaling:${var.aws_region}:${var.aws_account_id}:autoScalingGroup:*:autoScalingGroupName/operations-*-jvx-asg",
-          "arn:aws:ec2:${var.aws_region}:${var.aws_account_id}:*"                    
+          "arn:aws:ec2:${var.aws_region}:${var.aws_account_id}:*",
+          "aws_launch_template.my_launch_template.id"
          ]
       }
 
